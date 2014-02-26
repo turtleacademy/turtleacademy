a0c98fc58ef9366a7daa920edff7422d6c23b241
lucio <webmaster@turtleacademy.com>
2014-02-26 23:05:03 +0800

    Footer partial cleanup

--- files/footer.php
+++ files/footer.php
@@ -1,46 +1,35 @@
     
 <?php
-        $currentFile    = $_SERVER["PHP_SELF"];
-        $parts          = Explode('/', $currentFile);
-        $currentPage    = $parts[count($parts) - 1];
-        $doc            = _('Doc'); 
-        $newLesson      = _('Create new Lesson');
-        $faq            = _('FAQ');
-        $donate         = _('Donate');
-        $contactUs      = _('Contact Us');
-        $turtleac       = _('TurtleAcademy');
-        if (!isset ($root_dir))
-             $root_dir = "/";
-        if (!isset ($locale))
-            $locale = "en_US";
+    //TODO - remove unneeded variable comments
+        //$currentFile    = $_SERVER["PHP_SELF"];
+        //$parts          = Explode('/', $currentFile);
+        //$currentPage    = $parts[count($parts) - 1];
+        //$newLesson      = _('Create new Lesson');
+        if (!isset ($root_dir)) $root_dir = "/";
+        //if (!isset ($locale)) $locale = "en_US";
         $footer =
-        "
-        <footer id='footer'>
-
+        "<footer id='footer'>
             <div id='footer_elem' lang='" . $lang ."'>
                 <ul>
-                    <li lang='" . $locale_domain ."'>
-                        <a id='doc' title='Project documentation' href='".$site_path."/project/doc/".$lang."'>
-                        <b>
-                        ". 
-                        $doc ."
-                        </b>
+                    <li>
+                        <a id='doc' title='"._('Project documentation')."' href='".$root_dir."project/doc/".$lang."'>
+                        <b>". _('Documentation') ."</b>
                         </a>
                     </li>  
-                    <li lang='" . $locale_domain ."'>
-                         <a href='".$root_dir."faq.php'><b>".$faq."</b></a>   
+                    <li>
+                         <a href='".$root_dir."faq.php'><b>"._('FAQ')."</b></a>  
                     </li>
-                    <li lang='" . $locale_domain ."'>                       
-                        <a href='mailto:support@turtleacademy.com'><b>".$contactUs."</b></a>              
+                    <li>                       
+                        <a href='mailto:support@turtleacademy.com'><b>"._('Contact Us')."</b></a>
                     </li>
-                    <li lang='" . $locale_domain ."'>
-                        <a href='".$root_dir."donate.php'><b>".$donate."</b></a>                      
+                    <li>
+                        <a href='".$root_dir."donate.php'><b>"._('Donate')."</b></a>
                     </li>
                 </ul>
              </div> 
              <div id='turtleAt'>
-                <p> &copy; 2013 " . $turtleac . " </p>
+                <p> &copy; 2014 " . _('TurtleAcademy') . " </p>
             </div>
-        </footer>            
-        "        
+        </footer>";       
+              
 ?>
