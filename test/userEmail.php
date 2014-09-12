d06935d89df7d38e69575167b933c7d525c0a28a
turtleacademy <webmaster@turtleacademy.com>
2014-09-12 13:37:26 +0800

    Change the files/registration folder to files/email , to have sagnificate

--- test/userEmail.php
+++ test/userEmail.php
@@ -1,19 +1,34 @@
 <?php
-    $phpDirPath = "../files/registration/inc/php/";
+    if (session_id() == '')
+        session_start();
+    $phpDirPath = "files/email/inc/php/";
     include_once $phpDirPath . 'config.php';
     include_once $phpDirPath . 'functions.php';
-    require_once("../environment.php");
-    require_once("../localization.php");
+    require_once ('environment.php');
+    require_once ("localization.php");
+    require_once("localization_js.php");
+    require_once ("files/cssUtils.php");
+    require_once ('files/openid.php');
+    require_once ('files/utils/topbarUtil.php');
     require_once("../localization_js.php");
     require_once("../files/utils/programUtil.php");
     require_once("../files/utils/userUtil.php");
+    
+    $directory = new RecursiveDirectoryIterator($phpDirPath);
+    $recIterator = new RecursiveIteratorIterator($directory);
+    $regex = new RegexIterator($recIterator, '/\/module.php$/i');
+
+    foreach($regex as $item) {
+       // include $item->getPathname();
+        echo $item->getPathname();
+    }
    
     $users      =   userUtil::get_varified_users_collection();
     $random     =   rand(1, 600);   
     $i          =   0;
     $userName   =   "";
     $email      =   "";
-    
+   
     foreach ($users as $user)
     {
         if ($i == $random)
