83f6e1db696ac5fe7f20ddf0e095b89eb384478f
turtleacademy <webmaster@turtleacademy.com>
2014-10-06 14:57:07 +0800

    Logging out using the new global variable

--- localization.php
+++ localization.php
@@ -27,7 +27,8 @@
         "ru" => "ru_RU",
         "he" => "he_IL",
         "zh" => "zh_CN",
-        "hr" => "hr_HR"
+        "hr" => "hr_HR",
+        "bg" => "bg_BG"
     );
     
     if (isset ($_GET['locale']))
@@ -78,12 +79,3 @@
     
     
 ?>
-    <!-- In order to infrom js files on which locale are we right now 
-    <script type="text/javascript">
-            var locale          = "<?php echo $locale_domain; ?>";
-            var localShort      =    locale.substr(0,2);
-            var rootDir         = "<?php echo $root_dir; ?>";
-            var sitePath        = "<?php echo $site_path; ?>";
-            var sitePathAlter   = "<?php echo $site_pate_with_www; ?>";
-    </script>
--->
