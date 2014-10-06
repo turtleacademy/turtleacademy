3f788db554791435ddbf2a70c782a0c6bc1f095b
turtleacademy <webmaster@turtleacademy.com>
2014-10-06 13:39:19 +0800

    Change user/profile URL to only Profile Using a global variable

--- environment.php
+++ environment.php
@@ -1,18 +1,19 @@
 <?php
-    // including some data to change between environments
     $db_name              =   "turtleTestDb";
     $db_lesson_collection =   "lessons";
     //$dbLessonCollection  =   "lessons_translate";
-    $db_progress_collection =   "user_progress_next";
+    $db_progress_collection =   "user_progress";
     //user_progress_next  =   "user_progress";
     //$site_path            =   "http://turtleacademy.com/next/";
     //$site_path          =   "http://turtleacademy.com/";
-    $site_path            =   "http://turtle.com/";
+    $site_path            =    "http://turtle.com/";
     //$site_pate_with_www   =    "http://www.turtleacademy.com/next/";  
-    $site_pate_with_www   =    "http://www.turtleacademy.com/";  
+    $site_pate_with_www   =    "http://www.turtle.com/";  
     $home_page            =   "index.php";
     $lesson_page          =   "learn.php";  
     $env                  =   "lcl";
     $root_dir             =   "/"; 
+    $minified             =   ".min";
+    $user_profile           =   "profile/";
     //$root_dir              = "/next/";
-?>  
+?>
